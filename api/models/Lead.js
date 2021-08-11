87b0acc7d2e86250f01aae9fd82cc97d3be0b5ab
Suprabhat <deadshot.sk0117@gmail.com>
2021-08-11 15:53:06 +0530

    Revert "Revert "Lead API and Pagination""
    This reverts commit fc40239602e6cd9963c734e6e61ed5861fe28eff.

---
+++ api/models/Lead.js
@@ -0,0 +1,63 @@
+/**
+ * Lead.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    fullName: {
+			type: 'string',
+			description: 'The full name for this user.',
+			required: true,
+			maxLength: 30,
+			example: 'Carol Reyna'
+		},
+
+		mobileNumber: {
+			type: 'string',
+			description: 'The username for this user.',
+			required: true,
+			maxLength: 10,
+			example: '8888888888'
+		},
+
+		emailId: {
+			type: 'string',
+			description: 'The username for this user.',
+			maxLength: 50,
+			example: 'carol.reyna@microsoft.com'
+		},
+
+		message: {
+			type: 'string',
+			description: 'Message entered by the lead',
+			maxLength: 500,
+			example: 'A51900027648'
+		},
+
+		
+
+		
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
